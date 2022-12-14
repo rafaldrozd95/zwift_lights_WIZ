79c37ce74b30bd3917700062922e21a76dae6a50
rafaldrozd95 <66203647+rafaldrozd95@users.noreply.github.com>
2022-12-14 19:53:31 +0100

    Update erg.py
    ghtreh

--- erg.py
+++ erg.py
@@ -1,6 +1,6 @@
 from scapy.all import sniff
 
-#added comment to solve conf
+#added comment to solve confgregre gerg ergergreger
 while True:
         a = []
         sniff(filter=f"dst port 3022", prn= lambda x: print(x))
